4b951b21cdf045e508e4d01328887f8ad61b0c60
monkey-H <5681713@qq.com>
2016-11-10 10:12:22 +0800

    merge nap-api & nap-core

--- orchestration/container_api/test/create_container.py
+++
@@ -1,6 +0,0 @@
-from api.container import Container
-
-container = Container.create_container(url='114.212.87.52:2376', image='ubuntu', command='/bin/sleep 30', version='1.21')
-print container.ip
-print container.cmd
-print container.ports
